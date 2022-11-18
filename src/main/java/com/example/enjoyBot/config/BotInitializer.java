78a94014437552c22064c7eb9d44f5655e8bd47a
kamenb <89181855033qQ>
2022-11-18 18:51:46 +0300

    Добавил начальную обработку логов и начал перенос на linux сервер

--- src/main/java/com/example/enjoyBot/config/BotInitializer.java
+++ src/main/java/com/example/enjoyBot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.example.enjoyBot.config;
 
 import com.example.enjoyBot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,6 +10,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class BotInitializer {
 
@@ -22,7 +24,7 @@ public class BotInitializer {
             telegramBotsApi.registerBot(bot);
         }
         catch (TelegramApiException exception) {
-
+            log.error("Error occured: " + exception.getMessage());
         }
     }
 }
