63583390d1824377c9d1f839bcecd9c5b1e724ee
kamenb <89181855033qQ>
2022-11-19 15:55:25 +0300

    Добавил главное меню и библиотеки для работы с базой данных MySql

--- src/main/java/com/example/enjoyBot/service/TelegramBot.java
+++ src/main/java/com/example/enjoyBot/service/TelegramBot.java
@@ -4,18 +4,38 @@ import com.example.enjoyBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
+    static final String HELP_TEXT = "Этот бот создан ради рахата Version: 0.0.1";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/java", "get info about Java language"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listOfCommands.add(new BotCommand("/help", "info how to use bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException exception) {
+            log.error("Error occured: " + exception);
+        }
     }
 
     @Override
@@ -38,10 +58,13 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
-                case "Java":
+                case "/java":
                     sendMessage(chatId, "Вот ссылки на некоторые ресурсы на тему Java: " + "\n" + "https://habr.com/ru/post/237043/");
                     break;
-                case "C#":
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+                case "/c#":
                     sendMessage(chatId, "C# принял ислам(((");
                     break;
                 default: sendMessage(chatId, "Sorry, command was not recognized...");
